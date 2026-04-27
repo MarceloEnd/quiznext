e179c353827d4227d498df770893d8dc421c599e
marcelohlendorf <marcel@adjust.com>
2026-04-27 10:10:24 +0200

    started with wordle

--- app/spiele/wordle/page.js
+++ app/spiele/wordle/page.js
@@ -24,21 +24,6 @@ export default function WordleOverviewSite() {
   return (
     <Box sx={{ minHeight: '100vh', bgcolor: '#fdfdfd' }}>
       <StandardHeader previousPath="/spiele"/>
-
-      <Container maxWidth="md" sx={{ mt: 4, pb: 10 }}>
-        <Paper elevation={4} sx={{ p: { xs: 3, sm: 6 }, textAlign: 'center', borderRadius: 4 }}>
-          <Typography
-            variant="h2"
-            color="primary"
-            gutterBottom
-            sx={{ fontWeight: 900, mb: 4, fontSize: { xs: '2.5rem', sm: '3.75rem' } }}
-          >
-            Wort Suche
-          </Typography>
-
-          <List sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-        </Paper>
-      </Container>
     </Box>
   );
 }
